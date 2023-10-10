662fdf9c9431e539dae6ef6be6e6f2c67ca61114
cronokun <crono.neko@gmail.com>
2023-10-10 13:05:35 +0300

    Initial commit

---
+++ lib/tabula.ex
@@ -0,0 +1,18 @@
+defmodule Tabula do
+  @moduledoc """
+  Documentation for `Tabula`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tabula.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tabula_test.exs
@@ -0,0 +1,8 @@
+defmodule TabulaTest do
+  use ExUnit.Case
+  doctest Tabula
+
+  test "greets the world" do
+    assert Tabula.hello() == :world
+  end
+end
