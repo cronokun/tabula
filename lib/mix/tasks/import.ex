8f2c5d6459d8fee530dec1e93b915c0055c39f28
cronokun <crono.neko@gmail.com>
2023-10-23 17:12:09 +0300

    Mix task to import board

---
+++ lib/mix/tasks/import.ex
@@ -0,0 +1,20 @@
+defmodule Mix.Tasks.Import do
+  @moduledoc ~S"""
+  Import cards from JSON file from Trello.
+
+      $ mix import PATH
+
+  It reads data from provided JSON file and creates cards in `priv/import/<board-name>/`.
+  If file already exists, it will be overwriten.
+  """
+
+  @shortdoc "Import cards from Trello"
+
+  use Mix.Task
+
+  @impl Mix.Task
+  def run([path | _rest]) do
+    Tabula.Import.run(path)
+    IO.puts("DONE!")
+  end
+end
