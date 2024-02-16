0f1bc24bd89eb2266a539ed79d67d55176410ee1
cronokun <crono.neko@gmail.com>
2024-02-16 20:54:36 +0300

    Introduce simple storage

--- lib/tabula/storage.ex
+++ lib/tabula/storage.ex
@@ -1,6 +1,9 @@
 defmodule Tabula.Storage do
   @moduledoc ~S"""
-  Simple API to store card's data.
+  Simple API to access cards data.
+
+  It's used to store card's data (like cover image, due dates and tags) when parsing markdown
+  and then retrieve that data when generating board's index page.
   """
 
   use Agent
@@ -9,14 +12,12 @@ defmodule Tabula.Storage do
     Agent.start_link(fn -> %{board_name: board_name, cards: %{}} end, name: __MODULE__)
   end
 
-  def add_card(card) do
-    Agent.update(__MODULE__, fn board ->
-      put_in(board, [:cards, card["title"]], card)
-    end)
+  def add_card(title, card) do
+    Agent.update(__MODULE__, fn board -> put_in(board, [:cards, title], card) end)
   end
 
   def get_card(card_name) do
-    Agent.get(__MODULE__, fn board -> Map.get(board.cards, card_name) end)
+    Agent.get(__MODULE__, fn board -> board.cards[card_name] end)
   end
 
   def board_name do
